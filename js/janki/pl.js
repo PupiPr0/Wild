c241d8899c021aae358c718ebff32d2195a6a388
Ryszard Kwiatkowski <140899316+PupiPr0@users.noreply.github.com>
2023-08-14 19:53:04 +0300

    Update pl.js

--- js/janki/pl.js
+++ js/janki/pl.js
@@ -1,23 +1,325 @@
-<div class="preloader" id="preload">
-    <svg xmlns:svg="http://www.w3.org/2000/svg" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.0" width="64px" height="64px" viewBox="0 0 128 128" xml:space="preserve"><rect x="0" y="0" width="100%" height="100%" fill="#FFFFFF" /><g><circle cx="16" cy="64" r="16" fill="#000000" fill-opacity="1"/><circle cx="16" cy="64" r="14.344" fill="#000000" fill-opacity="1" transform="rotate(45 64 64)"/><circle cx="16" cy="64" r="12.531" fill="#000000" fill-opacity="1" transform="rotate(90 64 64)"/><circle cx="16" cy="64" r="10.75" fill="#000000" fill-opacity="1" transform="rotate(135 64 64)"/><circle cx="16" cy="64" r="10.063" fill="#000000" fill-opacity="1" transform="rotate(180 64 64)"/><circle cx="16" cy="64" r="8.063" fill="#000000" fill-opacity="1" transform="rotate(225 64 64)"/><circle cx="16" cy="64" r="6.438" fill="#000000" fill-opacity="1" transform="rotate(270 64 64)"/><circle cx="16" cy="64" r="5.375" fill="#000000" fill-opacity="1" transform="rotate(315 64 64)"/><animateTransform attributeName="transform" type="rotate" values="0 64 64;315 64 64;270 64 64;225 64 64;180 64 64;135 64 64;90 64 64;45 64 64" calcMode="discrete" dur="720ms" repeatCount="indefinite"></animateTransform></g></svg>
-</div>
+/*
+ * Preloader (pure js) - 1.0.2
+ * A javascript library to preload images from HTML and CSS.
+ */
 
-<style>
-    .preloader{
-        position: fixed;
-        z-index: 1;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        height: 100%;
-        width: 100%;
-        background-color: #fff;
+(function (root, factory) {
+  if (typeof define === 'function' && define.amd) { // AMD
+      define(factory);
+  } else if (typeof exports === 'object') { // Node, CommonJS
+      module.exports = factory();
+  } else {
+      root.Preloader = factory; // Browser (window)
+  }
+}(this, function() {
+  'use strict';
+
+  var $ = {};
+
+  $.defaults = {
+    debug: false, // boolean
+    getFromCSS: true, // boolean
+    filesToLoad: [], // array
+    allowed: ['jpg', 'jpeg', 'png', 'gif'], // array
+    loadDelay: 0, // ms
+
+    beforeLoad: function() {},
+    afterLoad: function() {},
+    onUpdate: function(percent) {}
+  };
+
+  $.regex = {
+    files: /(href="(.*?)")|(src="(.*?)")|(url\((.*?)\))/g, // get string from url({*}), src="{*}", href="{*}"
+    junk: /(&quot;)|(')|(")/g, // clear string from ugly chars
+    breaks: ["2", "4", "6"] // regex.files breaks, 2:href, 4:src, 6:url
+  };
+
+  $.html = String(document.querySelector("html").innerHTML);
+  $.css = [];
+
+  var loadFunctions = [];
+
+  // constructor
+  var init = function(customOptions) {
+    try {
+      $.options = mergeOptions(customOptions || {});
+
+      checkPromiseSupport();
+
+      if ($.options.debug) { console.time('Preloader'); }
+
+      if ($.options.filesToLoad.length) {
+        $.filelist = [];
+
+        $.filelist = $.options.filesToLoad.filter(function(el) {
+          return filter(el) === 'image';
+        });
+
+        if (images.length === 0 && $.options.debug) throw 'Wrong defined images format.';
+      } else {
+        getImages(function(matches) {
+          // remove duplicates
+          $.filelist = matches.filter(function(x, y) {
+            return matches.indexOf(x) === y;
+          });
+        });
+
+      };
+
+      beforeLoad();
+
+    } catch (err) {
+      console.log(err);
     }
-</style>
+  };
 
-<script>
-        window.onload = function(){
-            let preloader = document.getElementById('preload');
-            preloader.style.display = 'none';
+  var mergeOptions = function(customOptions) {
+    for(var key in customOptions) {
+      if(typeof $.defaults[key] !== 'undefined') {
+        $.defaults[key] = customOptions[key];
+      }
+    }
+
+    return $.defaults;
+  };
+
+
+
+  var checkPromiseSupport = function() {
+    if(typeof Promise !== "undefined" && Promise.toString().indexOf("[native code]") !== -1) {
+      $.promise = true;
+    } else {
+      $.promise = false;
+    }
+  };
+
+  var getImages = function(callback) {
+    var matches = [];
+
+    // exec html with regex
+    while(match = $.regex.files.exec($.html)) {
+      for (var i in $.regex.breaks) {
+        if(typeof match[$.regex.breaks[i]] !== 'undefined') {
+          var match = match[$.regex.breaks[i]].replace($.regex.junk, "");
+
+          if(filter(match) === 'image') {
+            matches.push(match);
+          } else if (filter(match) === 'css') {
+            $.css.push(match);
+          }
+        }
+      };
+    }
+
+
+    if($.options.getFromCSS) {
+      var request = [];
+
+      for (var i = $.css.length - 1; i >= 0; i--) {
+        (function(i) {
+          request[i] = new XMLHttpRequest();
+          request[i].open("GET", $.css[i], true);
+          request[i].send();
+
+          request[i].onreadystatechange = function() {
+            if (request[i].readyState == 4 && request[i].status == 200) {
+              while (match = $.regex.files.exec(request[i].responseText)) {
+                var match = match[6].replace($.regex.junk, "");
+                if (filter(match) === 'image') {
+                  matches.push(match);
+                }
+              }
+
+              if (!i) {
+                callback(matches);
+              }
+            }
+          }
+        })(i);
+      };
+    } else {
+      callback(matches);
+    }
+  };
+
+  // filter matches from regex
+  var filter = function (file) {
+    var getType = /([^.;+_]+)$/.exec(file),
+        fileType = getType && getType[1];
+
+    if ($.options.allowed.indexOf(fileType) > -1) {
+      return 'image';
+    } else {
+      return fileType;
+    }
+  };
+
+  var beforeLoad = function() {
+    if (typeof $.options.beforeLoad == 'function') {
+      $.options.beforeLoad();
+    }
+
+    load();
+  }
+
+
+  var load = function() {
+    var promise, loaded = 0, percent = 0;
+
+    /*
+     * checks if promise is supported
+     */
+    if ($.promise) {
+      /*
+       * main loading function
+       * Map filelist and call function loadImage on each image that's returnig a Promise
+       * Returns variable with all Promises
+       */
+
+      // map filelist
+      var files = $.filelist.map(function (src) {
+        // call function loadImage on each image
+        return loadImage(src).then(function(image) {
+
+          // update percentage on resolve
+          loaded++;
+          updatePercentage(loaded);
+
+          if($.options.debug) { console.log(image.src); };
+        }, function(image) {
+
+          // update percentage on reject
+          loaded++;
+          updatePercentage(loaded);
+
+          if ($.options.debug) { console.log("No image: " + image.src); };
+        });
+      });
+
+      /*
+       * When all Promises are resolved call afterLoad function
+       */
+      Promise.all(files).then(function () {
+        $.complete = true;
+        afterLoad();
+      });
+    } else {
+      /*
+       * if promise is not supported
+       */
+
+      $.loadedImages = 0;
+      checkIfAllLoaded();
+
+      for (var i = $.filelist.length - 1; i >= 0; i--) {
+        loadImage($.filelist[i]);
+      }
+    }
+  };
+
+  var loadImage = function(src) {
+
+    /*
+     * checks if promise is supported
+     */
+    if ($.promise) {
+      return new Promise(function(resolve, reject) {
+        // create image object
+        var image = new Image();
+        image.src = src;
+
+        // handle events
+        image.onload = function() {
+          resolve(image);
         };
-</script>
+
+        image.onerror = function() {
+          reject(image);
+        };
+      });
+
+    } else {
+      /*
+       * if Promise is not supported
+       */
+
+      // create image object
+      var image = new Image();
+      image.src = src;
+
+      // handle events
+      image.onload = function() {
+        $.loadedImages++;
+        updatePercentage($.loadedImages);
+
+        if($.options.debug) { console.log(image.src); };
+      };
+
+      image.onerror = function() {
+        $.loadedImages++;
+        updatePercentage($.loadedImages);
+
+        if($.options.debug) { console.log("No image: " + image.src); };
+      };
+    }
+  };
+
+  var updatePercentage = function(loaded) {
+    var percent = Number(parseFloat((loaded / $.filelist.length) * 100).toFixed(2));
+    $.options.onUpdate(percent);
+  };
+
+  // function that checks if all files are loaded
+  var checkIfAllLoaded = function() {
+    var checkIfLoaded = setInterval(function() {
+      if ($.loadedImages === $.filelist.length) {
+        $.complete = true;
+        afterLoad();
+
+        clearInterval(checkIfLoaded);
+      }
+    }, 50);
+  };
+
+  /*
+   * afterLoad function
+   * Function that is started when preloader is done loading
+   * It's removing onload attr from images in DOM and it's starting function defined in config and functions added by onLoad function (with delay if defined in config)
+   */
+
+  var afterLoad = function() {
+    setTimeout(function() {
+      // call standard function defined in config
+      $.options.afterLoad();
+
+      // call functions added by onLoad function
+      for (var i = loadFunctions.length - 1; i >= 0; i--) {
+        loadFunctions[i]();
+      }
+    }, $.options.loadDelay);
+
+    if($.options.debug) { console.timeEnd('Preloader'); };
+  };
+
+
+
+  var onLoad = function(fn) {
+    // check if preloader is done loading, if so call function
+    if ($.complete) {
+      fn();
+    } else {
+      // if preloader is still loading images, add function to array of functions that will be fired when preloader is done loading
+
+      if(typeof fn == 'function') {
+        loadFunctions.push(fn);
+      }
+    }
+  };
+
+  return {
+    preloader: $,
+    init: init,
+    onLoad: onLoad
+  };
+
+}()));
