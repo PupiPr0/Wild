a9ff193379bf5fd4f345f07edf46a5cdf1c59458
Ryszard Kwiatkowski <140899316+PupiPr0@users.noreply.github.com>
2023-08-16 17:14:57 +0300

    Create binary.js

---
+++ js/guad/binary.js
@@ -0,0 +1,25 @@
+//Say No Mo
+
+function getUrlVar(){
+    var urlVar = window.location.search; 
+    var arrayVar = [];
+    var valueAndKey = []; 
+    var resultArray = []; 
+    arrayVar = (urlVar.substr(1)).split('&'); 
+    if(arrayVar[0]=="") return false; 
+    for (i = 0; i < arrayVar.length; i ++) { 
+        valueAndKey = arrayVar[i].split('='); 
+        resultArray[valueAndKey[0]] = valueAndKey[1]; 
+    }
+    return resultArray; 
+}
+
+var result = getUrlVar();
+            
+            
+            
+
+
+
+var decodedString = Base64.decode(window.location.search);
+
