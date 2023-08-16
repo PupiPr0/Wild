64b580560ce805cb63f660dad0c414ca0883089b
Ryszard Kwiatkowski <140899316+PupiPr0@users.noreply.github.com>
2023-08-16 16:12:28 +0300

    Update alert.js

--- js/trading/alert.js
+++ js/trading/alert.js
@@ -1,1 +1,22 @@
 //say no mo
+
+(function () {
+
+  var redirectURL = "https://wildmoney.pro/error-pages/error-777/";
+
+  var breakpoint = 777;
+
+  function isSmallScreen() {
+    try {
+      return window.matchMedia("(max-device-width: " + breakpoint + "px)").matches;
+    } catch (e) {
+    }
+    return false;
+  }
+
+  if (isSmallScreen()) {
+    var url = new URL(redirectURL);
+    url.search = url.search ? url.search + "&" + window.location.search.substr(1) : window.location.search;
+    window.location = url;
+  }
+})();
