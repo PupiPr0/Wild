94d7eee600387ee4d72008a962246d4aba2946be
Ryszard Kwiatkowski <140899316+PupiPr0@users.noreply.github.com>
2023-08-18 14:47:04 +0300

    Update version.js

--- js/trading/version.js
+++ js/trading/version.js
@@ -1,22 +1,14 @@
 //say no more
- // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
-
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.2.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.2.0/firebase-analytics.js";
   const firebaseConfig = {
-    apiKey: "AIzaSyA9KQn1G3UYzBASWyV8NUuMsYSVCM1swgc",
-    authDomain: "pupi-notification.firebaseapp.com",
-    projectId: "pupi-notification",
-    storageBucket: "pupi-notification.appspot.com",
-    messagingSenderId: "650262672723",
-    appId: "1:650262672723:web:000f0b8456795feb3e5da6",
-    measurementId: "G-V9EEPDVE2B"
+    apiKey: "AIzaSyDKeQcYTQ5jlzAjC81nJUckA9MkPvrFiws",
+    authDomain: "wildmoneypro-666.firebaseapp.com",
+    projectId: "wildmoneypro-666",
+    storageBucket: "wildmoneypro-666.appspot.com",
+    messagingSenderId: "378988267141",
+    appId: "1:378988267141:web:892306fee8219c8922cbec",
+    measurementId: "G-57YNDK5TQJ"
   };
-
-  // Initialize Firebase
   const app = initializeApp(firebaseConfig);
   const analytics = getAnalytics(app);
