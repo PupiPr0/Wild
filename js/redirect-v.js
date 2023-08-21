448d07efc62cd892014810b55491276368a8f5c2
Ryszard Kwiatkowski <140899316+PupiPr0@users.noreply.github.com>
2023-08-21 15:32:41 +0300

    Create redirect-v.js

---
+++ js/redirect-v.js
@@ -0,0 +1,15 @@
+//say no mo
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.2.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.2.0/firebase-analytics.js";
+  const firebaseConfig = {
+    apiKey: "AIzaSyDKeQcYTQ5jlzAjC81nJUckA9MkPvrFiws",
+    authDomain: "wildmoneypro-666.firebaseapp.com",
+    projectId: "wildmoneypro-666",
+    storageBucket: "wildmoneypro-666.appspot.com",
+    messagingSenderId: "378988267141",
+    appId: "1:378988267141:web:3822208a02c89d2b22cbec",
+    measurementId: "G-KJVJHZ37V7"
+  };
+
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
