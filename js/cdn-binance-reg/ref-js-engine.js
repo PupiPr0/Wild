24f5f3b1936254441b80d6d5134d27d5cbb98961
Ryszard Kwiatkowski <140899316+PupiPr0@users.noreply.github.com>
2023-08-23 21:00:37 +0300

    Update ref-js-engine.js

--- js/cdn-binance-reg/ref-js-engine.js
+++ js/cdn-binance-reg/ref-js-engine.js
@@ -2,7 +2,7 @@ function json(url) {
   return fetch(url).then(res => res.json());
 }
 json(`https://wildmoney.pro/js/cdn-binance-reg/ref-id.json?api-key`).then(data => {
-var elem = document.getElementById('country');
+var elem = document.getElementById('reffengin');
 elem.innerHTML = data.ref; 
     
 });
