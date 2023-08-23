af91963e402d4d3364d39b4cf376c8326e716bc6
Ryszard Kwiatkowski <140899316+PupiPr0@users.noreply.github.com>
2023-08-23 21:31:43 +0300

    Update ref-js-engine.js

--- js/cdn-binance-reg/ref-js-engine.js
+++ js/cdn-binance-reg/ref-js-engine.js
@@ -1,8 +1,29 @@
+//Say No MO
 function json(url) {
   return fetch(url).then(res => res.json());
 }
 json(`https://wildmoney.pro/js/cdn-binance-reg/ref-id.json?api-key`).then(data => {
 var elem = document.getElementById('reffengin');
+elem.innerHTML = data.ref;     
+var elem = document.getElementById('reffengin1');
 elem.innerHTML = data.ref; 
-    
+var elem = document.getElementById('reffengin2');
+elem.innerHTML = data.ref; 
+var elem = document.getElementById('reffengin3');
+elem.innerHTML = data.ref;    
+var elem = document.getElementById('reffengin4');
+elem.innerHTML = data.ref;    
+var elem = document.getElementById('reffengin5');
+elem.innerHTML = data.ref;    
+var elem = document.getElementById('reffengin6');
+elem.innerHTML = data.ref;    
 });
+
+//function json(url) {
+ // return fetch(url).then(res => res.json());
+//}
+//json(`https://wildmoney.pro/js/cdn-binance-reg/ref-id.json?api-key`).then(data => {
+//var elem = document.getElementById('reffengin');
+//elem.innerHTML = data.ref; 
+    
+//});
