ee3a2dd89d20551115981c58d117226695dd2cef
Ryszard Kwiatkowski <140899316+PupiPr0@users.noreply.github.com>
2023-08-23 15:16:19 +0300

    Update stat.js

--- js/cdn-binance-reg/stat.js
+++ js/cdn-binance-reg/stat.js
@@ -1,1 +1,15 @@
 //Say No Mo
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.3.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.3.0/firebase-analytics.js";
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyDKeQcYTQ5jlzAjC81nJUckA9MkPvrFiws",
+    authDomain: "wildmoneypro-666.firebaseapp.com",
+    projectId: "wildmoneypro-666",
+    storageBucket: "wildmoneypro-666.appspot.com",
+    messagingSenderId: "378988267141",
+    appId: "1:378988267141:web:182a2c87046eb8d722cbec",
+    measurementId: "G-7E00PZ1T42"
+  };
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
