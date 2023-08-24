16197c39edd9d81f4d1a68ce34baf678512deee9
Ryszard Kwiatkowski <140899316+PupiPr0@users.noreply.github.com>
2023-08-24 19:54:44 +0300

    Add files via upload

---
+++ cdn/repo-content-media/binance-reg-page/refferal-page/gotofiles/cookies.js
@@ -0,0 +1,45 @@
+function createCookie(name, value, days) {
+    var expires;
+
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toGMTString();
+    } else {
+        expires = "";
+    }
+    document.cookie = encodeURIComponent(name) + "=" + encodeURIComponent(value) + expires + "; path=/";
+}
+
+function readCookie(name) {
+    var nameEQ = encodeURIComponent(name) + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) === ' ')
+            c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) === 0)
+            return decodeURIComponent(c.substring(nameEQ.length, c.length));
+    }
+    return null;
+}
+
+function eraseCookie(name) {
+    createCookie(name, "", -1);
+}
+
+if (isDetail == 1 && post_code > 0) {
+    var recaptchaReadycallback = function () {
+        setTimeout(
+                function ()
+                {
+                    grecaptcha.render('google-recaptcha-cm-js', {
+                        sitekey: keyCapcha,
+                        callback: function (response) {
+                        }
+                    });
+
+                }, 1000);
+
+    };
+}
