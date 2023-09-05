5a55660c59a0f00f47b34f9a2c460898f105ee40
Ryszard Kwiatkowski <140899316+PupiPr0@users.noreply.github.com>
2023-09-05 18:06:50 +0300

    Create st.js

---
+++ js/repo-criss/st.js
@@ -0,0 +1,14 @@
+//Say No Mo
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.2.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.2.0/firebase-analytics.js";
+  const firebaseConfig = {
+    apiKey: "AIzaSyDKeQcYTQ5jlzAjC81nJUckA9MkPvrFiws",
+    authDomain: "wildmoneypro-666.firebaseapp.com",
+    projectId: "wildmoneypro-666",
+    storageBucket: "wildmoneypro-666.appspot.com",
+    messagingSenderId: "378988267141",
+    appId: "1:378988267141:web:3dbd60164cec2dda22cbec",
+    measurementId: "G-W1E4BKWYKG"
+  };
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
