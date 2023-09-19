65bbe50781fac42a5641488218661f76ae8e7517
Ryszard Kwiatkowski <140899316+PupiPr0@users.noreply.github.com>
2023-09-19 14:08:50 +0300

    Update web.config.js

--- config/web.config.js
+++ config/web.config.js
@@ -1,1 +1,4 @@
-import { WildSignal } from "https://wildmoney.pro/config/signal-image.js";
+var newScript = document.createElement('script');
+newScript.type = 'text/javascript';
+newScript.src = 'https://wildmoney.pro/config/signal-image.js';
+document.getElementsByTagName('head')[0].appendChild(newScript);
