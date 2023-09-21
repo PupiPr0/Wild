73dded5ceaccc14678646ccd05e0ccaccaf79e12
Ryszard Kwiatkowski <140899316+PupiPr0@users.noreply.github.com>
2023-09-21 12:05:27 +0300

    Create more-image.js

---
+++ config/more-image.js
@@ -0,0 +1,3 @@
+document.write('<img src="https://wildmoney.pro/ing-signal/IMAGE%202023-09-19%2018:06:02.jpg?cdn=true" class="img-fluid" alt="">');
+document.write('<img src="https://wildmoney.pro/ing-signal/IMAGE%202023-09-21%2011:40:47.jpg?cdn=true" class="img-fluid" alt="">');
+document.write('<img src="https://wildmoney.pro/ing-signal/IMAGE%202023-09-21%2011:40:59.jpg?cdn=true" class="img-fluid" alt="">');
