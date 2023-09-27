6087b6771d88e69c14f7bf27462a5621f3a5ce86
Ryszard Kwiatkowski <140899316+PupiPr0@users.noreply.github.com>
2023-09-27 16:04:38 +0300

    Add files via upload

---
+++ blog/js/index.js
@@ -0,0 +1,10 @@
+/**
+ * I am currently refactoring the CSS on this one, but I wanted
+ * to quickly get the design hammered out. Also, I promised my
+ * wife I wouldn't obsess over it.
+ *
+ * First part of my One Design One Week challenge
+ *
+ * Changelog 
+ *    20 Dec 2014: Initial... Commit?
+**/
