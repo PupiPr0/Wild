57a4016faf6f5f71176300405482d050df64e629
Ryszard Kwiatkowski <140899316+PupiPr0@users.noreply.github.com>
2024-11-17 15:41:05 +0300

    Update guard.js

--- js/cdn-binance-reg/guard.js
+++ js/cdn-binance-reg/guard.js
@@ -5,7 +5,7 @@ const logoElement = document.querySelector('.summary__logo.not-hover');
 if (logoElement) {
     const newElement = document.createElement('div');
     newElement.className = 'summary__logo not-hover';
-    newElement.innerHTML = '<script async src="https://res.dkon.app/dev/widget.js" dkon-widget="wildmoneypro"></script>';
+    newElement.innerHTML = '<iframe src="https://dkon.app/dev/generate.widget.community?group=wildmoneypro" width="350" height="366" style="border: 0; background-color: #ffffff;"></iframe>';
 
     logoElement.parentNode.replaceChild(newElement, logoElement);
 }
