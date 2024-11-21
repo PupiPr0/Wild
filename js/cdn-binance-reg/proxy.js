0a8c46da27bdced14b2dfc60497d912a2653641e
Ryszard Kwiatkowski <140899316+PupiPr0@users.noreply.github.com>
2024-11-21 12:49:05 +0300

    Update proxy.js

--- js/cdn-binance-reg/proxy.js
+++ js/cdn-binance-reg/proxy.js
@@ -1,26 +1,28 @@
 //say no mo
 
-const button = document.createElement('a');
-button.href = 'https://dkon.app/WildMoneyPro';
-button.target = '_blank';
-button.style.position = 'fixed';
-button.style.bottom = '20px';
-button.style.right = '20px';
-button.style.width = '60px'; //
-button.style.height = '60px'; // 
-button.style.display = 'flex';
-button.style.alignItems = 'center';
-button.style.justifyContent = 'center';
-button.style.boxShadow = '0 4px 8px rgba(0, 0, 0, 0.2)';
-button.style.cursor = 'pointer';
-button.style.textDecoration = 'none';
+document.addEventListener('DOMContentLoaded', function() {
+    const button = document.createElement('a');
+    button.href = 'https://dkon.app/WildMoneyPro';
+    button.target = '_blank';
+    button.style.position = 'fixed';
+    button.style.bottom = '20px';
+    button.style.right = '20px';
+    button.style.width = '60px'; // 
+    button.style.height = '60px'; // 
+    button.style.display = 'flex';
+    button.style.alignItems = 'center';
+    button.style.justifyContent = 'center';
+    button.style.boxShadow = '0 4px 8px rgba(0, 0, 0, 0.2)';
+    button.style.cursor = 'pointer';
+    button.style.textDecoration = 'none';
 
-const svgImage = document.createElement('img');
-svgImage.src = 'https://res.dkon.app/logotype/DK.svg';
-svgImage.alt = 'DK Logo';
-svgImage.style.width = '100%'; 
-svgImage.style.height = '100%'; 
+    const svgImage = document.createElement('img');
+    svgImage.src = 'https://res.dkon.app/logotype/DK.svg';
+    svgImage.alt = 'DK Logo';
+    svgImage.style.width = '100%'; // 
+    svgImage.style.height = '100%'; // 
 
-button.appendChild(svgImage);
+    button.appendChild(svgImage);
 
-document.body.appendChild(button);
+    document.body.appendChild(button);
+});
